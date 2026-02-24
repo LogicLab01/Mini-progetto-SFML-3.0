25ba8a7e402c073c1bf5a81d781be6b580b0ba9f
LogicLab01 <fede.armida@gmail.com>
2026-02-24 20:44:33 +0100

    Aggiunte le collisioni, un ostacolo e ottimizzazione della finestra di gioco

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <SFML/Graphics.hpp>
 
 class Game {
@@ -8,12 +7,17 @@ public:
 
 private:
     void processEvents();
-    void update(float dt);
+    void update(sf::Time dt);
     void render();
+    void handleCollision(sf::Vector2f lMove);
+    void handleWindowCollision(); // Nuova collisione bordi
 
-    sf::RenderWindow m_window;
-    sf::RectangleShape m_player;
-    float m_speed{ 300.f };
+    sf::RenderWindow window;
+    sf::View view; // Per mantenere le proporzioni
+    sf::RectangleShape player;
+    sf::RectangleShape wall;
+    sf::Vector2f movement;
+    sf::Clock clock;
+    
+    const float speed = 400.f;
 };
-
-
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,48 +1,88 @@
 #include "../include/Game.hpp"
 
 Game::Game() 
-    : m_window(sf::VideoMode({800, 600}), "SFML 3.0 Game") {
-    
-    m_player.setSize({50.f, 50.f});
-    m_player.setFillColor(sf::Color::Green);
-    m_player.setPosition({400.f, 300.f});
+    : window(sf::VideoMode({800, 600}), "Gioco") {
+    window.setFramerateLimit(144);
+
+    // Inizializziamo la vista con la dimensione originale
+    view.setSize({800.f, 600.f});
+    view.setCenter({400.f, 300.f});
+
+    player.setSize({50.f, 50.f});
+    player.setFillColor(sf::Color::Green);
+    player.setPosition({100.f, 100.f});
+
+    wall.setSize({100.f, 100.f});
+    wall.setFillColor(sf::Color::Red);
+    wall.setPosition({400.f, 300.f});
 }
 
 void Game::run() {
-    sf::Clock clock;
-    while (m_window.isOpen()) {
-        // Calcolo Delta Time
-        float dt = clock.restart().asSeconds();
-
+    while (window.isOpen()) {
+        sf::Time deltaTime = clock.restart();
         processEvents();
-        update(dt);
+        update(deltaTime);
         render();
     }
 }
 
 void Game::processEvents() {
-    while (const std::optional event = m_window.pollEvent()) {
+    while (const std::optional event = window.pollEvent()) {
         if (event->is<sf::Event::Closed>()) {
-            m_window.close();
+            window.close();
+        }
+        // Evita distorsioni al ridimensionamento
+        else if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+            // Aggiorniamo la viewport per mantenere il rapporto d'aspetto
+            sf::FloatRect visibleArea({0.f, 0.f}, {(float)resized->size.x, (float)resized->size.y});
+            window.setView(sf::View(visibleArea)); 
+            // Nota: se vuoi che il mondo resti della stessa dimensione "zoomata", 
+            // usa view e calcola la viewport proporzionale.
         }
     }
-}
 
-void Game::update(float dt) {
-    sf::Vector2f movement{0.f, 0.f};
-
-    // Sintassi SFML 3.0: sf::Keyboard::Key::...
+    movement = {0.f, 0.f};
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) movement.y -= 1.f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) movement.y += 1.f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) movement.x -= 1.f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) movement.x += 1.f;
+}
 
-    // Applichiamo il movimento corretto con dt
-    m_player.move(movement * m_speed * dt);
+void Game::update(sf::Time deltaTime) {
+    sf::Vector2f frameMovement = movement * speed * deltaTime.asSeconds();
+    
+    player.move(frameMovement);
+    
+    handleCollision(frameMovement);
+    handleWindowCollision(); // Controlla i bordi dopo il movimento
+}
+
+void Game::handleWindowCollision() {
+    sf::Vector2f pos = player.getPosition();
+    sf::Vector2f size = player.getSize();
+    // Otteniamo la dimensione attuale della finestra/vista
+    sf::Vector2f winSize = window.getView().getSize();
+
+    // Bordi orizzontali
+    if (pos.x < 0) pos.x = 0;
+    if (pos.x + size.x > winSize.x) pos.x = winSize.x - size.x;
+
+    // Bordi verticali
+    if (pos.y < 0) pos.y = 0;
+    if (pos.y + size.y > winSize.y) pos.y = winSize.y - size.y;
+
+    player.setPosition(pos);
+}
+
+void Game::handleCollision(sf::Vector2f lastMovement) {
+    if (auto intersection = player.getGlobalBounds().findIntersection(wall.getGlobalBounds())) {
+        player.move(-lastMovement);
+    }
 }
 
 void Game::render() {
-    m_window.clear(sf::Color::Black); // Se vedi nero, il ciclo funziona
-    m_window.draw(m_player);          // Disegna il quadrato verde
-    m_window.display();
+    window.clear();
+    window.draw(wall);
+    window.draw(player);
+    window.display();
 }
